95b6081b6e105ef5ab8917d623bde955225d561d
Harrison Dutson <totodutson@gmail.com>
2023-05-08 16:11:15 -0600

    artist finished

---
+++ sql-basics/artist.sql
@@ -0,0 +1,12 @@
+INSERT INTO artist (name)
+VALUES ('kupla'),
+	('game_chops'),
+  ('purple-cat');
+
+SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+SELECT * FROM artist ORDER BY name LIMIT 5;
+
+SELECT * FROM artist WHERE name LIKE 'Black%';
+
+SELECT * FROM artist WHERE name LIKE '%Black%';
